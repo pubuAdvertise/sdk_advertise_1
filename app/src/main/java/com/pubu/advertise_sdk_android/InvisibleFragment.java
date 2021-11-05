c4d5068ff353a2ccca2769aaa2eea3446e261e00
pubuAdvertise <pubu_advertise@163.com>
2021-11-05 09:08:36 +0800

    5

---
+++ app/src/main/java/com/pubu/advertise_sdk_android/InvisibleFragment.java
@@ -0,0 +1,7 @@
+package com.pubu.advertise_sdk_android;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+public class InvisibleFragment extends Fragment {
+}
